ee9ab4a52d03fdee70587cd3535f0457a7cd7831
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-04-17 15:39:55 -0400

    Move clues to definition file

--- src/pages/clue/[id].tsx
+++ src/pages/clue/[id].tsx
@@ -1,3 +1,5 @@
+import CLUES from '../../definitions/clueData'
+
 function Clue({ clueData }) {
     return (
         <main className="flex min-h-screen flex-col items-center pt-24 text-center">
@@ -26,62 +28,5 @@ export async function getStaticProps({ params }) {
         },
     };
 }
-
-
-
-
-const CLUES = [
-    {
-        id: '1',
-        clue: 'Among the men who have walked on the Moon, his last name is alphabetically first',
-        answer: 'Buzz Aldrin',
-    },
-    {
-        id: '2',
-        clue: 'Known for more philosophical works, he wrote the play "La Mandragola", in which Florentines are rewarded for immoral actions',
-        answer: 'Machiavelli',
-    },
-    {
-        id: '3',
-        clue: 'Number of Canadian provinces that border the Great Lakes',
-        answer: '1',
-    },
-    {
-        id: '4',
-        clue: 'A part of this marine mammal was prized by medieval folk, who thought it belonged to a unicorn',
-        answer: 'Narwhal',
-    },
-    {
-        id: '5',
-        clue: 'Displayed in a new case in September 1997, it is the centerpiece of the Harry Winston Gallery',
-        answer: 'The hope diamond',
-    },
-
-
-
-    {
-        id: '6',
-        clue: 'Number of Canadian provinces that border the Great Lakes',
-        answer: '1',
-    },
-
-    {
-        id: '7',
-        clue: 'Among the men who have walked on the Moon, his last name is alphabetically first',
-        answer: 'Buzz Aldrin',
-    },
-
-    {
-        id: '8',
-        clue: 'Number of Canadian provinces that border the Great Lakes',
-        answer: '1',
-    },
-    {
-        id: '9',
-        clue: 'Displayed in a new case in September 1997, it is the centerpiece of the Harry Winston Gallery',
-        answer: 'The hope diamond',
-    },
-]
-
 
 export default Clue
