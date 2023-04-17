c475798d2a738a6b76fbd0ac6a409b73925cebed
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-04-17 11:54:20 -0400

    Add heading, CSS adjustments

---
+++ src/pages/clue.tsx
@@ -0,0 +1,7 @@
+export default function Clue() {
+    return (
+      <main className="flex min-h-screen flex-col items-center pt-24 text-center bg-green-100">
+          <p>this is the clue page</p>
+      </main>
+    )
+  }
