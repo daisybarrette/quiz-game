a4a51ae45e4f99a2ae67affc2e1eef175d302977
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-05-10 15:05:48 -0400

    Display player score

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Head from 'next/head'
 
+import { PlayerContext } from './PlayerContext'
 
 type LayoutProps = {
     pageTitle: string;
     children: React.ReactNode;
 }
 
-const Layout = ({ pageTitle, children }: LayoutProps) => (
-    <>
-        <Head>
-            <title>{pageTitle}</title>
-            <meta property="og:title" content={pageTitle} key="title" />
-        </Head>
+const Layout = ({ pageTitle, children }: LayoutProps) => {
+    const { gameState, } = useContext(PlayerContext);
 
-        <div className='min-h-screen flex flex-col'>
-            {children}
+    const scoreMessage = gameState.correct.length === 0 && gameState.incorrect.length === 0
+        ? 'No answers yet'
+        : `Correct: ${gameState.correct.length} Incorrect: ${gameState.incorrect.length}`
 
-            <footer className='mt-auto self-center'>
-                <p className='p-6'>
-                    by <a className="underline hover:no-underline hover:bg-yellow-50 hover:text-black" href='https://www.daisybarrette.com/'>Daisy Barrette</a> on <a className="underline hover:no-underline hover:bg-yellow-50 hover:text-black" href='https://github.com/daisybarrette/quiz-game'>GitHub</a>
-                </p>
-            </footer>
-        </div>
-    </>
+    return (
+        <>
+            <Head>
+                <title>{pageTitle}</title>
+                <meta property="og:title" content={pageTitle} key="title" />
+            </Head>
 
-)
+            <div className='min-h-screen flex flex-col'>
+                <aside className='rounded-sm shadow-md shadow-black block w-[calc(12em+2vw)] ml-auto sticky top-0 p-2 bg-lightest-yellow text-center text-black text-shadow-none'>
+                    {scoreMessage}
+                </aside>
+
+                {children}
+
+                <footer className='mt-auto self-center'>
+                    <p className='p-6'>
+                        by <a className="underline hover:no-underline hover:bg-yellow-50 hover:text-black" href='https://www.daisybarrette.com/'>Daisy Barrette</a> on <a className="underline hover:no-underline hover:bg-yellow-50 hover:text-black" href='https://github.com/daisybarrette/quiz-game'>GitHub</a>
+                    </p>
+                </footer>
+            </div>
+        </>
+    )
+}
 
 export default Layout
