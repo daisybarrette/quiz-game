b58e1f21b8f1fa205bdeb8465c20f15d746d32dd
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-05-10 12:42:58 -0400

    Adjust responsive sizing

--- src/pages/clue/[id].tsx
+++ src/pages/clue/[id].tsx
@@ -22,10 +22,10 @@ function Clue({ clueData }: { clueData: ClueType }) {
             pageTitle="Trivia Game | Clue"
         >
             <main className="flex flex-col items-center text-center">
-                <h1 className="text-2xl mt-24 p-10 w-9/12">{`${clueData.clue}`}</h1>
+                <h1 className="mt-[calc(1rem_+_10vh+5vw)] w-9/12 text-2xl ">{`${clueData.clue}`}</h1>
 
                 <div
-                    className="flex flex-col justify-center w-[calc(12rem_+_20vw)] h-[calc(8rem_+_20vh)]"
+                    className="flex flex-col justify-center w-[calc(12rem_+_20vw)] h-[calc(4rem_+_18vh)]"
                 >
                     <button
                         className={`${hasRevealBeenClicked ? 'hidden' : 'block'} py-8 px-10 ${primaryLinkStyles}`}
@@ -45,7 +45,7 @@ function Clue({ clueData }: { clueData: ClueType }) {
                     </p>
                 </div>
 
-                <div className='flex items-center  h-[calc(1rem_+_8vh)]'>
+                <div className='flex items-center h-[calc(1rem_+_8vh)]'>
                     <button
                         className={`${hasRevealBeenClicked && gameState.unanswered.includes(clueData.id) ? 'block' : 'hidden'} py-8 px-10 ${primaryLinkStyles}`}
                         onClick={() => {
@@ -76,7 +76,7 @@ function Clue({ clueData }: { clueData: ClueType }) {
                         {'Wrong'}
                     </button>
 
-                    <p className={`${hasRevealBeenClicked && !gameState.unanswered.includes(clueData.id) ? 'block' : 'hidden'} text-xl`}>
+                    <p className={`${hasRevealBeenClicked && !gameState.unanswered.includes(clueData.id) ? 'block' : 'hidden'} text-m`}>
                         {`Answer marked ${gameState.correct.includes(clueData.id) ? 'correct' : 'incorrect'}`}
                     </p>
                 </div>
